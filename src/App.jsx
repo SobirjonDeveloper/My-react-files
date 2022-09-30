c123fdbbe4e70d10fdfcb4964f4d93237bd70aff
Sobirjon <sobirjonfayzullayev32@gmail.com>
2022-09-30 21:55:37 +0500

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,64 @@
-import GreenChild from "./green-child";
-import GreenParent from "./green-parent";
+import React, { Component } from "react";
+import "./style.css";
+// import React, { Component } from "react";
 
-const App = () => {
-  return (
-    <div style={{ margin: "30px auto", width: "60%" }}>
-      <GreenParent />
-    </div>
-  );
-};
-
-export default App;
+export default class Line extends Component {
+  render() {
+    return (
+      <div className="Line">
+        <ifram src="sobir" frameborder="1">
+          <tr>
+            <td>ID</td>
+            <td>NAME</td>
+            <td>AGE</td>
+            <td>ADDRESS</td>
+            <td>STATUS</td>
+            <td>NICKNAME</td>
+            <td>UNIV</td>
+            <td>EDIT</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>Sobirjon Fayzullayev</td>
+            <td>26</td>
+            <td>Toshkent Uzbekistan</td>
+            <td>Student</td>
+            <td>Ali</td>
+            <td>sejong</td>
+            <td>EDIT</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>Odina Saidnazarova</td>
+            <td>15</td>
+            <td>Toshkent Uzbekistan</td>
+            <td>Student</td>
+            <td>Bonu</td>
+            <td>Sejong</td>
+            <td>EDIT</td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td>SHoaziz Xusanov</td>
+            <td>16</td>
+            <td>Toshkent Uzbekistan</td>
+            <td>student</td>
+            <td>Xusanov</td>
+            <td>Sejong</td>
+            <td>EDIT</td>
+          </tr>
+          <tr>
+            <td>9</td>
+            <td>Abduqodir Abdullayev</td>
+            <td>15</td>
+            <td>ToshkentUzbekistan</td>
+            <td>student</td>
+            <td>Abduqodir</td>
+            <td>Sejong</td>
+            <td>EDIT</td>
+          </tr>
+        </ifram>
+      </div>
+    );
+  }
+}
