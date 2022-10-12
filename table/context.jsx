40ea494637381fd73891803c23178a56fee8a6a0
Sobirjon <sobirjonfayzullayev32@gmail.com>
2022-10-12 22:17:11 +0500

    comment

---
+++ table/context.jsx
@@ -0,0 +1,13 @@
+// import React, { createContext, useState } from "react";
+// import { ContextData } from "./ContextData";
+
+// export const Boylik = createContext();
+
+// const TableContext = ({ children }) => {
+//   const [state, setState] = useState(ContextData);
+//   return (
+//     <Boylik.Provider value={[state, setState]}>{children}</Boylik.Provider>
+//   );
+// };
+
+// export default TableContext;
---
+++ table/table.jsx
@@ -0,0 +1,19 @@
+// import React, { useContext } from "react";
+// import { ContextData } from "./ContextData";
+// import { WrapNavbar } from "./contextstyle";
+// import { Boylik } from "./TableContext";
+// const ContextNavbar = () => {
+//   const [state, setState] = useContext(Boylik);
+//   return (
+//     <div>
+//       <WrapNavbar>
+//         <h1>Home</h1>
+//         <h1>About</h1>
+//         <h1>contact</h1>
+//         <h1>{state.length}</h1>
+//       </WrapNavbar>
+//     </div>
+//   );
+// };
+
+// export default ContextNavbar;
