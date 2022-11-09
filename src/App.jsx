a5cda42de7734a5bd166921ef55e58ecd7997597
Sobirjon <sobirjonfayzullayev32@gmail.com>
2022-11-09 10:16:47 +0500

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
 import React from "react";
-import Hih from "./house";
+import { Student } from "./Data";
 import "./style.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      Student: Student,
+    };
   }
   render() {
     return (
-      <div className="wrapper">
-        <Hih
-          title="New Apartment Nice Wiev"
-          title2="Quincy ST, Brooklyn, NY,"
-          title3="$2,800/mo"
-          title4="$7500/mo"
-        />
-        <Hih
-          title="New Apartment Nice Wiev"
-          title2="Quincy ST, Brooklyn, NY,"
-          title3="$2,800/mo"
-          title4="$7500/mo"
-        />
+      <div>
+        {this.state.Student.map((std) => {
+          return (
+            <table border={1}>
+              {/* <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PHONE</th>
+                <th>STATUS</th>
+              </tr> */}
+              <tr className="wrap">
+                <td width={50}>{std.id}</td>
+                <td width={300}>{std.name}</td>
+                <td width={200}>{std.phone}</td>
+                <td width={130}>{std.status}</td>
+                <td width={200}>
+                  <button
+                    onClick={() => {
+                      let res = this.state.Student.filter(
+                        (value) => value.id !== Student.id
+                      );
+                      this.setState({ Student: res });
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </table>
+          );
+        })}
       </div>
     );
   }
